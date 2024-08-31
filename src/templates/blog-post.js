04849d2a2f46678809e268431738129a206f5b38
Duru Kizito <Kizito2nv@gmail.com>
2024-08-31 10:31:31 +0100

    feted

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -27,7 +27,7 @@ export const BlogPostTemplate = ({
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
-           : null}
+          
           </div>
         </div>
       </div>
